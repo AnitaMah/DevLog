e15687188aee607a051646d04384d418d2d80fdf
AnitaMah <140920875+AnitaMah@users.noreply.github.com>
2026-07-18 13:39:12 +0200

    Refactor main.dart to use DatabaseHelper
    Removed Hive initialization code and added DatabaseHelper initialization.

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:dev_log/models/module.dart';
+import 'package:dev_log/models/submodule.dart';
 import 'package:dev_log/models/user_model.dart';
 import 'package:dev_log/screens/dashboard_screen.dart';
 import 'package:dev_log/theme/app_theme.dart';
+import 'package:dev_log/database/database_helper.dart';
 
 void main() async {
-  // 1. Обов'язково для ініціалізації Flutter-біндінгів
   WidgetsFlutterBinding.ensureInitialized();
-
-  // 2. Ініціалізація Hive
-  await Hive.initFlutter();
-
-  // 3. Реєстрація адаптерів
-  // Важливо: ці адаптери створюються автоматично командою 'flutter pub run build_runner build'
-  Hive.registerAdapter(ModuleAdapter()); 
-  Hive.registerAdapter(UserModelAdapter());
-
-  // 4. ВІДКРИТТЯ БОКСІВ
-  await Hive.openBox<Module>('modules');
-  await Hive.openBox<UserModel>('userBox');
-
+  await DatabaseHelper.init();
   runApp(const DevLogApp());
 }
 
@@ -35,7 +24,7 @@ class DevLogApp extends StatelessWidget {
       theme: ThemeData(
         brightness: Brightness.dark,
         scaffoldBackgroundColor: AppColors.background,
-        fontFamily: 'Roboto', 
+        fontFamily: 'Roboto',
         colorScheme: const ColorScheme.dark(
           primary: AppColors.accentPurple,
           surface: AppColors.background,
