4e97928ad4c627b7c618350f075c660735bbb539
AnitaMah <140920875+AnitaMah@users.noreply.github.com>
2026-07-21 18:50:35 +0200

    Fix import formatting and clean up comments

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,12 @@ import 'package:hive_flutter/hive_flutter.dart';
 import 'package:dev_log/models/module.dart';
 import 'package:dev_log/models/submodule.dart';
 import 'package:dev_log/models/user_model.dart';
-import 'package:dev_log/screens/home_screen.dart'; // <-- Переконайтеся, що цей імпорт є
+import 'package:dev_log/screens/home_screen.dart';
 import 'package:dev_log/database/database_helper.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await DatabaseHelper.init(); // Ініціалізація Hive
+  await DatabaseHelper.init();
   runApp(const DevLogApp());
 }
 
@@ -28,7 +28,7 @@ class DevLogApp extends StatelessWidget {
           titleTextStyle: TextStyle(color: Colors.white, fontSize: 20),
         ),
       ),
-      home: const HomeScreen(), // <-- Головний екран
+      home: const HomeScreen(), // Головний екран
     );
   }
 }
